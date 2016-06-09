a4426cf9fbfa09824a3b5106f2ebfbb06f590943
Malcolm Reid Jr <reid-jr@rinnegan.cs.wisc.edu>
2016-06-09 10:57:56 -0500

    Initial commit. Basic authentication popup that calls into the ParseCommandLine SWAMP API

---
+++ eclipseplugin/src/eclipseplugin/dialogs/AuthenticationDialog.java
@@ -0,0 +1,100 @@
+/* Malcolm Reid Jr.
+ * 06/07/2016
+ * UW SWAMP
+ * AuthenticationDialog.java
+ * Code to implement a dialog box for getting SWAMP Authentication information
+ */
+
+package eclipseplugin.dialogs;
+
+import java.util.ArrayList;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+import edu.wisc.cs.swamp.ParseCommandLine;
+import edu.wisc.cs.swamp.*;
+import edu.uiuc.ncsa.swamp.session.*;
+
+public class AuthenticationDialog extends TitleAreaDialog {
+	private Text usernameText;
+	private Text passwordText;
+	private String username;
+	private String password;
+	private static final String AUTHENTICATION_PROMPT = "Please enter your authentication information for the SWAMP.";
+	private static final String INVALID_MESSAGE = "Invalid username or password.";
+	
+	public AuthenticationDialog(Shell parentShell) {
+		super(parentShell);
+	}
+		
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		// TODO Increase width of username and password text fields
+		Composite area = (Composite) super.createDialogArea(parent);
+		Composite container = new Composite(area, SWT.NONE);
+		
+		this.setTitle("Swamp Authentication");
+		this.setMessage(AUTHENTICATION_PROMPT);
+		
+		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		GridLayout layout = new GridLayout(2, false);
+		container.setLayout(layout);
+		
+		Label usernameLabel = new Label(container, SWT.NONE);
+		usernameLabel.setText("Username: ");
+		usernameText = new Text(container, SWT.SINGLE | SWT.BORDER);
+		GridData griddata = new GridData();
+		griddata.grabExcessHorizontalSpace = true;
+		griddata.horizontalAlignment = GridData.FILL;
+		container.setLayoutData(griddata);
+		
+		Label passwordLabel = new Label(container, SWT.NONE);
+		passwordLabel.setText("Password: ");
+		passwordText = new Text(container, SWT.PASSWORD | SWT.BORDER);
+		container.setLayoutData(griddata);
+		
+		return area;
+	}
+	
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, "Login", true);
+		createButton(parent, IDialogConstants.CANCEL_ID, "Cancel", false);
+	}
+	
+	
+	@Override
+	protected void okPressed() {
+		username = usernameText.getText();
+		password = passwordText.getText();
+		if ((username.length() == 0) || (password.length() == 0)) {
+			this.setMessage(INVALID_MESSAGE + "\n" + AUTHENTICATION_PROMPT);
+			usernameText.setText("");
+			passwordText.setText("");
+			return;
+		}
+		System.out.println("Username: " + username + "\tPassword: " + password);
+		// do the validation with the API using the things in the text fields
+		//ArrayList<Session> sessions;
+		//sessions = ParseCommandLine.getCurrSessionInfo();
+		int val = ParseCommandLine.testAPI(3);
+		System.out.println(val);
+		//sessions = ParseCommandLine.getSessions(username, password);
+		//System.out.println("Is sessions null? " + sessions == null);
+		//System.out.println("Number of sessions: " + sessions.size());
+		//if (sessions.size() < 1) {
+			
+			super.okPressed();
+		//}
+		//else {
+			// add error message here
+			//this.setMessage(INVALID_MESSAGE + "\n" + AUTHENTICATION_PROMPT);
+		//}
+	}
+	
+}
